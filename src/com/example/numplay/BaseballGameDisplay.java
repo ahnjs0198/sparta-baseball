910061c955b200382fe6b703047138685633a324
ahnjs0198 <ahnjs0198@gmail.com>
2024-10-22 21:44:49 +0900

    결과값 출력 개선

--- src/com/example/numplay/BaseballGameDisplay.java
+++ src/com/example/numplay/BaseballGameDisplay.java
@@ -2,6 +2,14 @@ package com.example.numplay;
 
 public class BaseballGameDisplay {
     public void displayHint (int strike, int ball){
-        System.out.println(strike + "스트라이크 "+ ball + "볼");
+        if (strike==0 && ball==0) {
+            System.out.println("아웃");
+        } else if (strike==0) {
+            System.out.println(ball + "볼");
+        } else if (ball==0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(strike + "스트라이크 "+ ball + "볼");
+        }
     }
 }
