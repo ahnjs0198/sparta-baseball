c39453bcb53386ff79d72e25d227fb1da7abada0
ahnjs0198 <ahnjs0198@gmail.com>
2024-10-23 10:00:15 +0900

    lv4 게임 난이도 조절 기능 추가

--- src/com/example/numplay/BaseballGame.java
+++ src/com/example/numplay/BaseballGame.java
@@ -12,8 +12,10 @@ public class BaseballGame {
     Scanner scanner = new Scanner(System.in);
     Scanner scanner2 = new Scanner(System.in);
     int count = 0;
+    int level = 3;
     BaseballGameDisplay hint = new BaseballGameDisplay();
     List<Integer> gameLog = new ArrayList<>();
+
 
 
     public BaseballGame() {
@@ -21,18 +23,30 @@ public class BaseballGame {
     }
 
     public int play() {
-
+        count = 0;
         while(true){
-            count = 0;
             System.out.println("환영합니다! 원하시는 번호를 입력해주세요");
-            System.out.println("1.게임 시작하기 2. 게임 기록 보기 3. 종료하기");
+            System.out.println("0. 자리수 설정 1.게임 시작하기 2. 게임 기록 보기 3. 종료하기");
             int option = scanner2.nextInt();
-            if (option==1){
+            // 0. 자리수 설정
+            if (option==0){
+                while(true){
+                    System.out.println("설정하고자 하는 자리수를 입력하세요.");
+                    level = scanner2.nextInt();
+                    if (level>2 && level<6){
+                        System.out.println(level+"자리수 난이도로 설정되었습니다.");
+                        break;
+                    } else {
+                        System.out.println("올바른 숫자를 입력해주세요!");
+                    }
+                }
+            } // 1.게임 시작하기
+            else if (option==1){
                 System.out.println();
                 System.out.println("< 게임을 시작합니다 >");
                 // 정답 answer을 설정
                 answerSet.clear();
-                while (answerSet.size()<3){
+                while (answerSet.size()<level){
                     answerSet.add(random.nextInt(8) + 1);
                 }
                 answerList = new ArrayList<>(answerSet);
@@ -44,7 +58,7 @@ public class BaseballGame {
                     System.out.println("숫자를 입력하세요");
                     String input = scanner.nextLine();
                     // 2. 올바른 입력값을 받았는지 검증
-                    while(!validateInput(input)){
+                    while(!validateInput(input, level)){
                         System.out.println("올바르지 않은 입력값입니다");
                         System.out.println();
                         System.out.println("숫자를 입력하세요");
@@ -53,25 +67,27 @@ public class BaseballGame {
                     // 3. 게임 진행횟수 증가
                     count++;
                     // 4. 스트라이크 개수 계산
-                    int strike = countStrike(input);
+                    int strike = countStrike(input, level);
                     // 5. 정답여부 확인, 만약 정답이면 break 를 이용해 반복문 탈출
-                    if (strike == 3){
+                    if (strike == level){
                         System.out.println("정답입니다!");
                         System.out.println("시도횟수 - "+count);
                         gameLog.add(count);
                         break;
                     }
                     // 6. 볼 개수 계산
-                    int ball = countBall(input);
+                    int ball = countBall(input, level);
                     // 7. 힌트 출력
                     hint.displayHint(strike, ball);
                 }
-            } else if (option==2){
+            } // 2. 게임 기록 보기
+            else if (option==2){
                 System.out.println(gameLog);
                 for (int i=0; i<gameLog.size(); i++){
                     System.out.println(i+1+"번째 게임 : 시도 횟수 - "+ gameLog.get(i));
                 }
-            }  else if (option==3){
+            } // 3. 종료하기
+            else if (option==3){
                 System.out.println("< 숫자 야구 게임을 종료합니다 >");
                 break;
             } else {
@@ -83,12 +99,12 @@ public class BaseballGame {
         return count;
     }
 
-    protected boolean validateInput(String input){
+    protected boolean validateInput(String input, int level){
         // 1부터 9사이의 숫자인지 확인
         if(input.matches("^[1-9]*$")){
             // 중복된 숫자가 없는지 확인
             int[] inputDigits = Stream.of(input.split("")).mapToInt(Integer::parseInt).toArray();
-            for (int i=0; i<3; i++){
+            for (int i=0; i<level; i++){
                 for (int j=0; j<i; j++){
                     if (inputDigits[i]==inputDigits[j]){
                         return false;
@@ -101,11 +117,11 @@ public class BaseballGame {
         }
     }
 
-    private int countStrike(String input){
+    private int countStrike(String input, int level){
         int[] inputDigits = Stream.of(input.split("")).mapToInt(Integer::parseInt).toArray();
         int count = 0;
         // 같은 숫자 같은 자리가 있는지 확인
-        for (int i=0; i<3; i++){
+        for (int i=0; i<level; i++){
             if (inputDigits[i]==answer[i]){
                 count++;
             }
@@ -113,11 +129,11 @@ public class BaseballGame {
         return count;
     }
 
-    private int countBall(String input){
+    private int countBall(String input, int level){
         int[] inputDigits = Stream.of(input.split("")).mapToInt(Integer::parseInt).toArray();
         int count = 0;
         // 같은 숫자 다른 자리가 있는지 확인
-        for (int i=0; i<3; i++){
+        for (int i=0; i<level; i++){
             for (int j=0; j<i; j++){
                 if(inputDigits[i]==answer[j]){
                     count++;
